70162c780b7f9bf33d9c7bb36181c93387869832
Vivian Zheng <vivianz38@nycstudents.net>
2024-04-02 17:32:21 +0000

    Tue, Apr 2, 2024, 1:32 PM -04:00

---
+++ freedom-project/freestyle/scripts.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const front = document.getElementById('front')
+const back = document.getElementById('back')
+const btn = document.getElementById('flip-btn')
+
+function handleFlip() {
+  front.classList.toggle('flipped')
+  back.classList.toggle('flipped')
+}
+
+btn.addEventListener('click', handleFlip)
